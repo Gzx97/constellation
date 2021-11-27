91317e37f2ef631ca5f7a539f154823e3ffe24b2
gzxmac <wanan499gzx@163.com>
2021-11-27 17:43:10 +0800

    ddas

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -3,12 +3,12 @@ const getBaseUrl = (url) => {
   if (process.env.NODE_ENV === 'development') {
     //开发环境 - 根据请求不同返回不同的BASE_URL
     // BASE_URL = 'http://47.52.21.155:8200/'
-    BASE_URL = 'http://118.31.109.21:8000/'
+    BASE_URL = 'https://www.starlucky.top/'
 
   } else {
     // 生产环境
 
-    BASE_URL = 'http://118.31.109.21:8000/'
+    BASE_URL = 'https://www.starlucky.top/'
 
   }
   return BASE_URL
